edfc539bc8e9c8c28387cc99f68c9cee377a9067
camillykahl <camillymescke@gmail.com>
2022-09-21 13:06:59 -0300

    novo commit

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
-import FuncionarioDAO
-import ClienteDAO
-import ProdutoDAO
+import db
+from mod_funcionario.FuncionarioModel import FuncionarioDB
+from mod_funcionario import FuncionarioDAO
+from mod_cliente.ClienteModel import ClienteDB
+from mod_cliente import ClienteDAO
+from mod_produto.ProdutoModel import ProdutoDB
+from mod_produto import ProdutoDAO
+#import mod_produto.ProdutoDAO as ProdutoDAO
 
 app = FastAPI()
 
 app.include_router(FuncionarioDAO.router)
 app.include_router(ClienteDAO.router)
 app.include_router(ProdutoDAO.router)
+
+db.criaTabelas()
